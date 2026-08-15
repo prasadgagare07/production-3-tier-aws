cf59281f5194957ba1d9d5046ff8458009d4d232
Prasad Gagare <prasadgagare91@gmail.com>
2026-08-15 23:03:23 +0530

    Add outputs for ALB DNS name and ARN values

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,1 +1,4 @@
-
+output "alb_dns_name" { value = aws_lb.this.dns_name }
+output "alb_zone_id" { value = aws_lb.this.zone_id }
+output "target_group_arn" { value = aws_lb_target_group.app.arn }
+output "alb_arn" { value = aws_lb.this.arn }
