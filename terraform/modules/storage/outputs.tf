8c2d182c624b969a5a1b8c260caa4da481287aff
Prasad Gagare <prasadgagare91@gmail.com>
2026-08-15 23:36:23 +0530

    Format outputs in outputs.tf for consistency

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -1,2 +1,7 @@
-output "bucket_name" { value = aws_s3_bucket.app.bucket }
-output "bucket_arn" { value = aws_s3_bucket.app.arn }
+output "bucket_name" {
+  value = aws_s3_bucket.app.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.app.arn
+}
