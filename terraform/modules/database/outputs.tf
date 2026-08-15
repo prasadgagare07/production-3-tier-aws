c67c2ecffde353af03e7753a7245b98bb23e0a49
Prasad Gagare <prasadgagare91@gmail.com>
2026-08-15 23:35:55 +0530

    Reformat outputs in outputs.tf
    Reformat output blocks for better readability.

--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -1,4 +1,15 @@
-output "db_endpoint" { value = aws_db_instance.this.address }
-output "db_port" { value = aws_db_instance.this.port }
-output "db_name" { value = aws_db_instance.this.db_name }
-output "secret_arn" { value = aws_secretsmanager_secret.db.arn }
+output "db_endpoint" {
+  value = aws_db_instance.this.address
+}
+
+output "db_port" {
+  value = aws_db_instance.this.port
+}
+
+output "db_name" {
+  value = aws_db_instance.this.db_name
+}
+
+output "secret_arn" {
+  value = aws_secretsmanager_secret.db.arn
+}
