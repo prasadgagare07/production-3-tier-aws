9bdb2172ede7ee4679902487f90e1dd210c34db1
Prasad Gagare <prasadgagare91@gmail.com>
2026-08-15 23:04:18 +0530

    Add variables for compute module in Terraform

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,1 +1,30 @@
-
+variable "project_name" { type = string }
+variable "vpc_id" { type = string }
+variable "private_app_subnet_ids" { type = list(string) }
+variable "ec2_sg_id" { type = string }
+variable "target_group_arn" { type = string }
+variable "db_host" { type = string }
+variable "db_secret_arn" { type = string }
+variable "s3_bucket_name" { type = string }
+variable "s3_bucket_arn" { type = string }
+variable "aws_region" { type = string }
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+variable "min_size" {
+  type    = number
+  default = 2
+}
+variable "max_size" {
+  type    = number
+  default = 4
+}
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
