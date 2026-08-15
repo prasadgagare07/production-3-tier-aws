5515b1dc46f64f3afc65b793d121200d0b6d2e2f
Prasad Gagare <prasadgagare91@gmail.com>
2026-08-15 23:07:23 +0530

    Add outputs for VPC and subnets in Terraform

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -1,1 +1,5 @@
-
+output "vpc_id" { value = aws_vpc.this.id }
+output "public_subnet_ids" { value = aws_subnet.public[*].id }
+output "private_app_subnet_ids" { value = aws_subnet.private_app[*].id }
+output "private_db_subnet_ids" { value = aws_subnet.private_db[*].id }
+output "vpc_cidr" { value = aws_vpc.this.cidr_block }
